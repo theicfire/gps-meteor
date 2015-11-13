371fafa87c1530347aca27b26fc8b2065c316cf0
Chase Lambert <chase@stripe.com>
2015-11-13 12:02:43 -0800

    Change phone

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ var PushBullet = Meteor.npmRequire('pushbullet');
 Meteor.npmRequire('log-timestamp'); // TODO can't mup do this for me?
 var client = Twilio('ACa8b26113996868bf72b7fab2a8ea0361', '47d7dc0b6dc56c2161dc44bc0324bb70');
 var last_ping;
-var MICRO_PHONE = '+16502356065';
-//var MICRO_PHONE = '+16507720745';
+//var MICRO_PHONE = '+16502356065';
+var MICRO_PHONE = '+16507720745';
 var WATCHDOG_TIMEOUT = 250000;
 var pusher = new PushBullet('oYHlSULc3i998hvbuVtsjlH0ps23l7y2');
 
