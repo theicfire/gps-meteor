77c65a4b8fbb1e0d913ec41c17b69091adfaf5ed
Chase Lambert <chase@stripe.com>
2015-12-01 18:27:33 -0800

    Phone number changed, watchdog timeout changed

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ var PushBullet = Meteor.npmRequire('pushbullet');
 Meteor.npmRequire('log-timestamp'); // TODO can't mup do this for me?
 var client = Twilio('ACa8b26113996868bf72b7fab2a8ea0361', '47d7dc0b6dc56c2161dc44bc0324bb70');
 var last_ping;
-//var MICRO_PHONE = '+16502356065';
-var MICRO_PHONE = '+16507720745';
-var WATCHDOG_TIMEOUT = 350000;
+var MICRO_PHONE = '+16502356065';
+//var MICRO_PHONE = '+16507720745'; // Stolen :(
+var WATCHDOG_TIMEOUT = 1800000;
 var pusher = new PushBullet('oYHlSULc3i998hvbuVtsjlH0ps23l7y2');
 var phone_action_map = {
   'lock':   '+15126435858',
